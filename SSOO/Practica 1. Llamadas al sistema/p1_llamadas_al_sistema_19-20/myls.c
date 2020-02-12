f063e875ce7bbf1063a9b3e9f5162c8d986b6fc7
CarlosCRO <carlosrubiolivares2000@gmail.com>
2020-02-12 18:31:07 +0100

    1º Version myls

--- SSOO/Practica 1. Llamadas al sistema/p1_llamadas_al_sistema_19-20/myls.c
+++ SSOO/Practica 1. Llamadas al sistema/p1_llamadas_al_sistema_19-20/myls.c
@@ -7,8 +7,34 @@
 
 int main(int argc, char *argv[])
 {
-	
 
+	DIR *descriptor;
+	//No hemos recibido argumentos, por lo que usamos el directorio actual
+	if(argc <2){
+		char buffer[PATH_MAX];
+		getcwd(buffer, PATH_MAX); //directorio guardado en buffer
+		descriptor = opendir (buffer);
+	}else if (argc == 2){
+	//Usamos el argumento como directorio a leer
+		descriptor = opendir(argv[1]);
+
+	}else{
+		printf("Exceso de argumentos\n");
+		return -1;
+	}
+	if (descriptor == NULL){
+		printf ("Error al abrir directorio \n");
+		return -1;
+	}
+	//Leemos cada uno de los ficheros a del directorio al que apunta descriptor
+	struct dirent *fichero_act;
+	while ( (fichero_act = readdir(descriptor) ) != NULL){
+		printf("%s\n",fichero_act->d_name);
+	}
+	//Hemos terminado de leer, cerramos el directorio
+	if (closedir(descriptor) == -1){
+		printf("Error al cerrar directorio\n");
+		return -1;
+	}
 	return 0;
 }
-
