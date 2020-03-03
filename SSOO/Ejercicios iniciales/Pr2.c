0a4b5a632ff53b24faf90d60bd92dd8298869a53
Jorge Rodríguez <jrf1616@gmail.com>
2020-03-03 18:18:19 +0100

    Correccion formato ejercicios iniciales.
    Former-commit-id: 4f89261dd89702f445352c3390deecdcee42b40b

--- SSOO/Ejercicios iniciales/Pr2.c
+++ SSOO/Ejercicios iniciales/Pr2.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-	char nom[5]; //Para que quepa Juan pero no si es mas largo no cabe
-	printf("Introduce nombre: ");
-	scanf("%s", nom);
-	printf("Hola %s. \n", nom);
-	return 0;
+int main() {
+  char nom[5]; // Para que quepa Juan pero no si es mas largo no cabe
+  printf("Introduce nombre: ");
+  scanf("%s", nom);
+  printf("Hola %s. \n", nom);
+  return 0;
 }
