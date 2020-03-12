109e7bf9b8144d29cd41ce20851bfb2dc74e6ed8
Jorge Rodríguez <jrf1616@gmail.com>
2020-03-12 19:21:07 +0100

    Codigo FSDB 1.2  VISTAS Y DISPARADORES DE HISTORICOS
    Former-commit-id: 34e254114f12e49fa08fa3f29354e19a8d3d498a

--- FBD/Practica 2/codigo.sql
+++ FBD/Practica 2/codigo.sql
@@ -40,10 +40,36 @@ ORDER BY
     avg(c)
     DESC fetch FIRST ROW ONLY;
 
-//AÑADIR AL DOCUMENTO, ES LA VISTA DE LEADER
+--AÑADIR AL DOCUMENTO, ES LA VISTA DE LEADER
 CREATE VIEW LEADER (nickname,club, media_comentarios) AS
     SELECT D.member, club, Z/M media_comentarios FROM (
     SELECT A.member, B.club, COUNT ('x') Z FROM PROPOSALS A JOIN COMMENTS B ON (A.title = B.title AND A.director = B.director) GROUP BY (A.member, B.club)) C JOIN
     (SELECT member, COUNT('x') M FROM PROPOSALS GROUP BY (member)) D ON (C.member = D.member)
     ORDER BY (media_comentarios) DESC
     FETCH FIRST 10 ROWS ONLY;
+
+--AÑADIR AL DOCUMENTO, SON VISTAS Y DISPARADORES DE HISTORICOS
+CREATE VIEW clubs_vigentes AS SELECT * FROM clubs WHERE (end_date IS NULL);
+/
+CREATE VIEW clubs_historicos AS SELECT * FROM clubs WHERE (end_date IS NOT NULL);
+/
+CREATE TRIGGER TG_not_insert_historicos instead OF INSERT ON clubs_historicos
+  DECLARE
+    notInsert Exception;
+  BEGIN
+    raise notInsert;
+  EXCEPTION
+    WHEN notInsert
+      THEN raise_application_error(-20001, 'No se puede insertar en historicos');
+    END TG_not_insert_historicos;
+/
+CREATE TRIGGER TG_delete_historicos instead OF DELETE ON clubs_historicos
+  BEGIN
+    DELETE FROM clubs WHERE(name=:OLD.name);
+  END TG_delete_historicos;
+/
+CREATE TRIGGER TG_delete_vigentes
+  instead OF DELETE ON clubs_vigentes
+  BEGIN
+    UPDATE clubs SET end_date=sysdate WHERE (name=:OLD.name);
+  END TG_delete_vigentes;
