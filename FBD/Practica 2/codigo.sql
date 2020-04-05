821f354c4a07d0a8a882021fd7a761168f247c57
CarlosCRO <carlosrubiolivares2000@gmail.com>
2020-04-05 19:59:07 +0200

    Codigo FSDB 1.5

--- FBD/Practica 2/codigo.sql
+++ FBD/Practica 2/codigo.sql
@@ -71,7 +71,7 @@ CREATE TRIGGER TG_delete_historicos instead OF DELETE ON clubs_historicos
 CREATE TRIGGER TG_delete_vigentes
   instead OF DELETE ON clubs_vigentes
   BEGIN
-    UPDATE clubs SET end_date=sysdate WHERE (name=:OLD.name);
+    UPDATE clubs SET end_date=sysdate WHERE (name =:OLD.name);
   END TG_delete_vigentes;
 
 --VIEW OPENPUB
@@ -106,3 +106,54 @@ CREATE ROLE usuarios_registrados NOT IDENTIFIED;
 GRANT select ON openpub TO usuarios_registrados;
 GRANT select ON any_goes TO usuarios_registrados;
 GRANT select ON REPORT TO usuarios_registrados;
+
+--DISPARADOR APPLICATION
+
+CREATE OR REPLACE TRIGGER APPLICATION BEFORE INSERT ON candidates FOR EACH ROW
+DECLARE
+    notInsert Exception;
+    l_exist varchar2(1);
+    BEGIN
+    select max('Y') into l_exist
+            from CLUBS
+            where (name = :new.club AND open = 'C');
+
+    IF :new.type = 'A' THEN
+        IF (l_exist is not null) THEN
+            raise notInsert;
+        END IF;
+        END  IF;
+    EXCEPTION
+    WHEN notInsert
+      THEN raise_application_error(-20002, 'No se puede solicitar acceso a un club cerrado sin invitacion');
+    END;
+/
+
+--PRUEBAS APPLICATION
+    --salta el disparador
+  insert into candidates (nick, club, type, req_date, req_msg, rej_date, rej_msg) values ('cmp', 'Guild of the Heart geraniums', 'A', to_date('10/01/19', 'DD/MM/YY'), 'dkhwizxcgqrjrfbf mudevgkdkj chwuka.',to_date('13/02/19', 'DD/MM/YY'),'dkhwizxcgqrjrfbf mudevgkdkj chwuka.');
+    --no salta el disparador
+insert into candidates (nick, club, type, req_date, req_msg, rej_date, rej_msg) values ('cmp', 'Guild of the Heart geraniums', 'I', to_date('10/01/19', 'DD/MM/YY'), 'dkhwizxcgqrjrfbf mudevgkdkj chwuka.',to_date('13/02/19', 'DD/MM/YY'),'dkhwizxcgqrjrfbf mudevgkdkj chwuka.');
+    --borrar la fila de la prueba
+delete from candidates where (nick = 'cmp' AND club = 'Guild of the Heart geraniums');
+
+--DISPARADOR OVERWRITE
+
+CREATE OR REPLACE TRIGGER OVERWRITE BEFORE INSERT ON comments FOR EACH ROW
+DECLARE
+    l_exist varchar2(1);
+    BEGIN
+    select max('Y') into l_exist
+            from COMMENTS
+            where (nick = :new.nick AND title = :new.title AND director = :new.director);
+
+    IF (l_exist is not null) THEN
+    delete from comments where (nick = :new.nick AND title = :new.title AND director = :new.director);
+    end if;
+
+    END overwrite;
+/
+
+--PRUEBAS OVERWRITE
+delete from comments where (message = 'Hola, esto es una prueba');
+insert into comments values ('The Sorority of Hamlet or all','ana',to_date('02/10/20','DD/MM/YY'),'Fantastic Mr. Fox','Wes Anderson','Luck','Hola, esto es una prueba','4');
