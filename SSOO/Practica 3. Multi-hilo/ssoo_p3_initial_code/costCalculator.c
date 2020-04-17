59b5c6a02549c00457a5c7099642a55faf8386ac
Jorge Rodríguez <jrf1616@gmail.com>
2020-04-17 23:03:26 +0200

    SO3 1.5
    Arreglado el calculo de indice, puntero compartido, repeticiones de 
    productor. Ademas del concepto del consumidor, con la estructura 
    exactamente igual que la anterior.

--- SSOO/Practica 3. Multi-hilo/ssoo_p3_initial_code/costCalculator.c
+++ SSOO/Practica 3. Multi-hilo/ssoo_p3_initial_code/costCalculator.c
@@ -14,7 +14,6 @@
 
 #define NUM_CONSUMERS 1
 
-
 /**
  * Entry point
  * @param argc
@@ -22,66 +21,93 @@
  * @return
  */
 
- pthread_mutex_t ring;
- pthread_cond_t lleno;
- pthread_cond_t vacio;
- struct queue *cola;
- const char *fichero;
+pthread_mutex_t ring;
+pthread_mutex_t des;
+pthread_cond_t lleno;
+pthread_cond_t vacio;
+struct queue *cola;
+const char *fichero;
+FILE *descriptorP;
+int total = 0;
 
+struct param {
+  int id_ini;
+  int op;
+};
 
+// Cada hilo tiene acceso a su propio dominio de id`s, donde el nº de operaciones que tendrá asociado será
+// floor(num.lineas/num.productores), leerá e insertará de uno en uno
+void *producir(void *arg) {
+  // Sacar parametros
+  struct param *p = arg;
 
- struct param{
-   int id_ini;
-   int op;
- };
+  // Hallamos el desciptor que corresponde al primer indice, para que no se mezclen bloquemas y desbloqueamos
+  pthread_mutex_lock(&des);
 
+  descriptorP = fopen(fichero, "r");
+  int counter = 0;
+  char chr;
+  while (counter <= p->id_ini) {
+    chr = fgetc(descriptorP);
+    if (chr == '\n') {
+      counter++;
+    }
+  }                            // fin while
+  FILE *current = descriptorP; // Almacenamos la posicion por la que va, de esta manera la podemos recuperar.
 
- //Cada hilo tiene acceso a su propio dominio de id`s, donde el nº de operaciones que tendrá asociado será
- //floor(num.lineas/num.productores), leerá e insertará de uno en uno
- void * producir(void *arg){
+  pthread_mutex_unlock(&des);
 
-   struct param *p= arg;
-   FILE* descriptorP = fopen (fichero, "r");
-   int counter = 0;
-   char chr;
-   while(counter <= p->id_ini){
-     chr = fgetc(descriptorP);
-     if(chr == '\n')
-     {
-       counter++;
-     }
-   }//fin while
+  int i, i1, i2 = 0;
+  for (int j = p->op; j > 0; j--) {
+    // Sacamos los valores a introducir en el buffer del puntero, y salvamos la nueva posicion del mismo.
+    pthread_mutex_lock(&des);
 
-   int i, i1, i2 = 0;
-   for (p->op; p->op == 0; p->op--){
+    descriptorP = current;
+    fscanf(descriptorP, "%d %d %d", &i, &i1, &i2);
+    current = descriptorP;
 
-     fscanf(descriptorP, "%d %d %d", &i, &i1, &i2);
-     struct element temporal = {i1,i2};
+    // Prueba impresion
+    // printf("Escribe el que comienza en %d, el id %d (Ajustado para coincidir)\n", p->id_ini, i-1);
 
-     pthread_mutex_lock(&ring);
-     while(queue_full(cola))
+    pthread_mutex_unlock(&des);
+
+    struct element temporal = {i1, i2};
+
+    pthread_mutex_lock(&ring);
+    while (queue_full(cola))
       pthread_cond_wait(&lleno, &ring);
 
-     queue_put(cola, &temporal);
-     pthread_cond_signal(&vacio);
-     pthread_mutex_unlock(&ring);
-   }
+    queue_put(cola, &temporal);
 
- }
+    pthread_cond_signal(&vacio);
+    pthread_mutex_unlock(&ring);
+  }
+}
 
- void * consumir(){
+void *consumir() {
+  struct element data;
+  //Bucle de todo mientras no se hallar leido todas las ops esperadas, meter parametro.
+  pthread_mutex_lock(&ring);
 
- }
+  while (queue_empty(cola))
+    pthread_cond_wait(&vacio, &ring);
 
-int calculo_lineas (const char filename[]){
-  FILE* file = fopen (filename, "r");
+  //Sacar parametros, queue_get
+
+  pthread_cond_signal(&lleno);
+  pthread_mutex_unlock(&ring);
+
+  //Al ser solo 1 la variable total no se modifica. Carlos <3
+  //total+=...
+}
+
+int calculo_lineas(const char filename[]) {
+  FILE *file = fopen(filename, "r");
   char chr;
   int lines = 0;
-  while(!feof(file))
-  {
+  while (!feof(file)) {
     chr = fgetc(file);
-    if(chr == '\n')
-    {
+    if (chr == '\n') {
       lines++;
     }
   }
@@ -89,35 +115,36 @@ int calculo_lineas (const char filename[]){
   return lines;
 }
 
-
 int main(int argc, const char *argv[]) {
-  int total = 0;
+
   printf("%s\n", argv[1]);
-  FILE* descriptor = fopen (argv[1], "r");
+  FILE *descriptor = fopen(argv[1], "r");
 
   int numVal;
-  fscanf(descriptor, "%d",&numVal);
+  fscanf(descriptor, "%d", &numVal);
   int numLin = calculo_lineas(argv[1]);
-  printf("Lineas: %d\n", numLin );
-  if(numVal>(numLin-1)){
+  printf("Lineas: %d\n", numLin);
+  if (numVal > (numLin - 1)) {
     perror("Error: Se esperaban mas operaciones.");
     return -1;
   }
 
   int productores = atoi(argv[2]);
-  if(productores<=0){
+  if (productores <= 0) {
     perror("Error: Numero invalido de productores.");
   }
   int size = atoi(argv[3]);
-  if(size<=0){
+  if (size <= 0) {
     perror("Error: Tamaño invalido.");
   }
+
   cola = queue_init(size);
   pthread_mutex_init(&ring, NULL);
+  pthread_mutex_init(&des, NULL);
   pthread_cond_init(&lleno, NULL);
   pthread_cond_init(&vacio, NULL);
 
-  int operaciones = floor(numVal/productores);
+  int operaciones = floor((numVal / productores));
   int id_inicio = 1;
   pthread_t hilosP[productores];
   pthread_t hiloC;
@@ -125,60 +152,57 @@ int main(int argc, const char *argv[]) {
   int i;
   fichero = malloc(sizeof(char[strlen(argv[1])]));
   fichero = argv[1];
-  //struct student *s = malloc( sizeof(*s) + sizeof(char [strlen(stud_name)])  );
   struct param args[productores];
-  for (i = 0; i<productores-1; i++){
-    args[i].op= operaciones;
-    args[i].id_ini= id_inicio;
+  for (i = 0; i < (productores - 1); i++) {
+    // printf("ID: %d\n", id_inicio);
+    args[i].op = operaciones;
+    args[i].id_ini = id_inicio;
     pthread_create(&hilosP[i], NULL, (void *)producir, &args[i]);
-    id_inicio+=operaciones;
+    id_inicio += operaciones;
   }
-  int  op_ultimo = numVal - (i*operaciones);
-  int id_ultimo = id_inicio;
-  args[productores-1].op = op_ultimo;
-  args[productores-1].id_ini = id_inicio;
-    pthread_create(&hilosP[productores-1],NULL, (void *)producir, &args[productores-1]);
+  int op_ultimo = numVal - (i * operaciones);
+  args[productores - 1].op = op_ultimo;
+  args[productores - 1].id_ini = id_inicio;
+  pthread_create(&hilosP[productores - 1], NULL, (void *)producir, &args[productores - 1]);
 
-  for (int i = 0; i<productores; i++){
+  for (int i = 0; i < productores; i++) {
     pthread_join(hilosP[i], NULL);
   }
   pthread_join(hiloC, NULL);
 
+  /*
+  printf("El numero total de productores es %d\n",numVal);
+  int i,i1,i2;
+  fscanf(descriptor, "%d %d %d", &i, &i1, &i2 );
+  printf("La primera fila es %d, %d y %d\n",i, i1, i2);
 
- /*
- printf("El numero total de productores es %d\n",numVal);
- int i,i1,i2;
- fscanf(descriptor, "%d %d %d", &i, &i1, &i2 );
- printf("La primera fila es %d, %d y %d\n",i, i1, i2);
+  fscanf(descriptor, "%d %d %d", &i, &i1, &i2 );
+  printf("La segunda fila es %d, %d y %d\n", i, i1, i2);
+ */
 
- fscanf(descriptor, "%d %d %d", &i, &i1, &i2 );
- printf("La segunda fila es %d, %d y %d\n", i, i1, i2);
-*/
+  /*
+    //PRUEBA BUFFER
+    printf("Inicio\n");
+    struct queue *cola = queue_init(3);
 
-/*
-  //PRUEBA BUFFER
-  printf("Inicio\n");
-  struct queue *cola = queue_init(3);
+    printf("He inicializado\n");
+    struct element elem = {1, 2};
 
-  printf("He inicializado\n");
-  struct element elem = {1, 2};
+    queue_put(cola, &elem);
 
-  queue_put(cola, &elem);
+    printf("Vacia %d\n", queue_empty(cola));
+    printf("Llena %d\n", queue_full(cola));
 
-  printf("Vacia %d\n", queue_empty(cola));
-  printf("Llena %d\n", queue_full(cola));
+    struct element *elem2 = queue_get(cola);
+    struct element elem3 = {10, 20};
 
-  struct element *elem2 = queue_get(cola);
-  struct element elem3 = {10, 20};
+    printf("Vacia %d\n", queue_empty(cola));
 
-  printf("Vacia %d\n", queue_empty(cola));
-
-  printf("Type %d, time %d\n", elem2->type, elem2->time);
-  printf("Type %d, time %d\n", elem3.type, elem3.time);
-  queue_destroy(cola);
-  //
-*/
-
+    printf("Type %d, time %d\n", elem2->type, elem2->time);
+    printf("Type %d, time %d\n", elem3.type, elem3.time);
+    queue_destroy(cola);
+    //
+  */
 
   printf("Total: %i €.\n", total);
   return 0;
