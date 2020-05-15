14a4e2e70531b1cec3b503d5c371bac402c36d33
xVenve <jrf1616@gmail.com>
2020-05-15 14:28:18 +0000

    Prueba bucles Python
    Imprime numero desde el 161 al 166116

--- Python/helloWorld.py
+++ Python/helloWorld.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
 numero= 16.61
-cadena1= "Hola, mundo "
-print(cadena1,numero)
+cadena1= 'Hola, mundo'
+print(cadena1, numero)
+cadena2='Texto de prueba de Python'
+print(cadena2)
+numeroTexto='Numero'
+numeros= ["Uno","Dos","Tres","Cuatro","Cinco"]
+for x in numeros:
+	print('\t', numeroTexto, x)
+
+print('Segundo bucle')
+
+for j in range(16,166116):
+	print(numeroTexto, j)
