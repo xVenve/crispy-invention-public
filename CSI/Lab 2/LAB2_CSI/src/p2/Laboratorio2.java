d21406e1b917549b63f67460142c4289e8ce38f2
Jorge Rodríguez <jrf1616@gmail.com>
2020-05-15 08:45:16 +0200

    CSI Lab2 1.4
    PostTest
    
    
    Former-commit-id: c6e23f44fc039c03ab34b139d7470919036954ac

--- CSI/Lab 2/LAB2_CSI/src/p2/Laboratorio2.java
+++ CSI/Lab 2/LAB2_CSI/src/p2/Laboratorio2.java
@@ -5,5 +5,8 @@ public class Laboratorio2 {
 	public static void main(String[] args) {
 		AES myAES = new AES();
 		myAES.doGenerateKey();
+		myAES.generateKeyAndIV();
+		myAES.doEncrypt();
+		myAES.doDecrypt();
 	}
 }
