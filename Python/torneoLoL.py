5df28bed260a727f4d2ed5c5aded047216d92935
Jorge Rodríguez <jrf1616@gmail.com>
2020-06-07 11:57:35 +0200

    Race condition fixed
    Check https://towardsdatascience.com/how-to-use-riot-api-with-python-b93be82dbbd6

--- Python/torneoLoL.py
+++ Python/torneoLoL.py
@@ -12,30 +12,33 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+lock = threading.Lock()
+
+
 def getRank(nick):
     url = 'https://euw.op.gg/summoner/userName=' + nick
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'lxml')
 
-    # Lista de tags pedidos
+    # List of tags
     tags = soup('div')
-    lst = list()
 
     # <div class="TierRank">Bronze 2</div>
 
     for j in tags:
-        k = re.findall(".*TierRank\">([A-Za-z]+\s[0-9])", str(j))
+        k = re.findall('.*TierRank">([A-Za-z]+\s[0-9])', str(j))
         if k:
+            lock.acquire()
             print('Rank', nick, ':\t \t ', k[0])
+            lock.release()
             break
 
 
 if __name__ == "__main__":
 
-
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('TorneoLoL-91e815443db5.json', scope)
-    client = gspread.authorize(creds)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('TorneoLoL-91e815443db5.json', scope)
+    client = gspread.authorize(credentials)
 
     sheet = client.open('SOLOQCHALLENGE').sheet1
 
@@ -46,9 +49,9 @@ if __name__ == "__main__":
         name = col[i]
         if not name:
             exit(0)
-        x = threading.Thread(target=getRank, args=(name,))
-        threads.append(x)
-        x.start()
+        exe = threading.Thread(target=getRank, args=(name,))
+        threads.append(exe)
+        exe.start()
 
     for i, thread in enumerate(threads):
         thread.join()
