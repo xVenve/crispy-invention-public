662c27e5a9222995a714aba72f1f23e126e8ec1f
Jorge Rodríguez <jrf1616@gmail.com>
2020-09-18 20:03:44 +0200

    AC Test input/output C++
    Former-commit-id: 252ae99c06a73ff7b5cbc388e88e0a89e9be8e1b

---
+++ AC/helloworld/inputOutput.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Jorge Rodríguez Fraile on 18/09/2020.
+//
+
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    cout <<"Introduzca su nombre y dos apellidos: \n";
+    string nombre, apellido1, apellido2;
+    cin >> nombre >> apellido1 >> apellido2;
+
+    cout <<"Introduzca su edad: \n";
+    int edad;
+    cin >>edad;
+
+    cout <<"Hola, " <<nombre <<" " <<apellido1 <<" "<<apellido2 <<" de " <<edad <<" años";
+    return 0;
+}
