14c1347e78b1babfc24412c60eabe8501e285a53
CarlosCRO <carlosrubiolivares2000@gmail.com>
2020-10-13 20:07:44 +0200

    Practica 2 IU 1.2 Implementación de navegación entre páginas mediante JavaScript
    Former-commit-id: 9c23728a80f5e276c9a6c5b7e1a69fa93a814df4

---
+++ IU/Practica 2/ej/script/script_main.js
@@ -0,0 +1,66 @@
+
+
+document.getElementById("linkforo_h").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_FORO").style.display = "block";
+  document.getElementById("CENTRAL_MAIN").style.display = "none";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+});
+
+
+document.getElementById("linkforo").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_FORO").style.display = "block";
+  document.getElementById("CENTRAL_MAIN").style.display = "none";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+});
+
+document.getElementById("linkcalificaciones").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "block";
+  document.getElementById("CENTRAL_MAIN").style.display = "none";
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+});
+
+document.getElementById("linkcalificaciones_h").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "block";
+  document.getElementById("CENTRAL_MAIN").style.display = "none";
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+});
+
+document.getElementById("linkestudiantes").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_LISTA").style.display = "block";
+  document.getElementById("CENTRAL_MAIN").style.display = "none";
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+});
+
+document.getElementById("linkestudiantes_h").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_LISTA").style.display = "block";
+  document.getElementById("CENTRAL_MAIN").style.display = "none";
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+});
+
+document.getElementById("linkmain").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+  document.getElementById("CENTRAL_MAIN").style.display = "block";
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+});
+
+document.getElementById("linkmain_h").addEventListener("click",
+function() {
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+  document.getElementById("CENTRAL_MAIN").style.display = "block";
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+});
