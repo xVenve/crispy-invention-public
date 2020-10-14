1a5b338415966809ddef46c25a7fcd2214ce4911
Jorge Rodríguez <jrf1616@gmail.com>
2020-10-14 10:53:11 +0200

    IU Practica 2 1.3
    Arreglada la pagina principal y hamburguesa.
    
    
    Former-commit-id: c01ec8a2a5cc1d67c3d0b18c650f5afa9d46b02e

--- IU/Practica 2/ej/script/script_main.js
+++ IU/Practica 2/ej/script/script_main.js
@@ -1,66 +1,72 @@
+document.addEventListener("DOMContentLoaded", start());
 
+function start() {
+  document.getElementById("CENTRAL_FORO").style.display = "none";
+  document.getElementById("CENTRAL_MAIN").style.display = "block";
+  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  document.getElementById("CENTRAL_LISTA").style.display = "none";
+}
 
 document.getElementById("linkforo_h").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_FORO").style.display = "block";
-  document.getElementById("CENTRAL_MAIN").style.display = "none";
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
-  document.getElementById("CENTRAL_LISTA").style.display = "none";
-});
-
+  function() {
+    document.getElementById("CENTRAL_FORO").style.display = "block";
+    document.getElementById("CENTRAL_MAIN").style.display = "none";
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+    document.getElementById("CENTRAL_LISTA").style.display = "none";
+  });
 
 document.getElementById("linkforo").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_FORO").style.display = "block";
-  document.getElementById("CENTRAL_MAIN").style.display = "none";
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
-  document.getElementById("CENTRAL_LISTA").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_FORO").style.display = "block";
+    document.getElementById("CENTRAL_MAIN").style.display = "none";
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+    document.getElementById("CENTRAL_LISTA").style.display = "none";
+  });
 
 document.getElementById("linkcalificaciones").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "block";
-  document.getElementById("CENTRAL_MAIN").style.display = "none";
-  document.getElementById("CENTRAL_FORO").style.display = "none";
-  document.getElementById("CENTRAL_LISTA").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "block";
+    document.getElementById("CENTRAL_MAIN").style.display = "none";
+    document.getElementById("CENTRAL_FORO").style.display = "none";
+    document.getElementById("CENTRAL_LISTA").style.display = "none";
+  });
 
 document.getElementById("linkcalificaciones_h").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "block";
-  document.getElementById("CENTRAL_MAIN").style.display = "none";
-  document.getElementById("CENTRAL_FORO").style.display = "none";
-  document.getElementById("CENTRAL_LISTA").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "block";
+    document.getElementById("CENTRAL_MAIN").style.display = "none";
+    document.getElementById("CENTRAL_FORO").style.display = "none";
+    document.getElementById("CENTRAL_LISTA").style.display = "none";
+  });
 
 document.getElementById("linkestudiantes").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_LISTA").style.display = "block";
-  document.getElementById("CENTRAL_MAIN").style.display = "none";
-  document.getElementById("CENTRAL_FORO").style.display = "none";
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_LISTA").style.display = "block";
+    document.getElementById("CENTRAL_MAIN").style.display = "none";
+    document.getElementById("CENTRAL_FORO").style.display = "none";
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  });
 
 document.getElementById("linkestudiantes_h").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_LISTA").style.display = "block";
-  document.getElementById("CENTRAL_MAIN").style.display = "none";
-  document.getElementById("CENTRAL_FORO").style.display = "none";
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_LISTA").style.display = "block";
+    document.getElementById("CENTRAL_MAIN").style.display = "none";
+    document.getElementById("CENTRAL_FORO").style.display = "none";
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  });
 
 document.getElementById("linkmain").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_LISTA").style.display = "none";
-  document.getElementById("CENTRAL_MAIN").style.display = "block";
-  document.getElementById("CENTRAL_FORO").style.display = "none";
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_LISTA").style.display = "none";
+    document.getElementById("CENTRAL_MAIN").style.display = "block";
+    document.getElementById("CENTRAL_FORO").style.display = "none";
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  });
 
 document.getElementById("linkmain_h").addEventListener("click",
-function() {
-  document.getElementById("CENTRAL_LISTA").style.display = "none";
-  document.getElementById("CENTRAL_MAIN").style.display = "block";
-  document.getElementById("CENTRAL_FORO").style.display = "none";
-  document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
-});
+  function() {
+    document.getElementById("CENTRAL_LISTA").style.display = "none";
+    document.getElementById("CENTRAL_MAIN").style.display = "block";
+    document.getElementById("CENTRAL_FORO").style.display = "none";
+    document.getElementById("CENTRAL_CALIFICACIONES").style.display = "none";
+  });
