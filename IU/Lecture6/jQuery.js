10b8744ad6979eef85c88452dcf5f8ca1c3bef8d
Jorge Rodríguez <jrf1616@gmail.com>
2020-10-22 11:59:00 +0200

    IU Lecture 6
    Former-commit-id: 77acb6439074603d52bcc305fd63bd32676aab13

---
+++ IU/Lecture6/jQuery.js
@@ -0,0 +1,26 @@
+$(____).action(____);
+    |  .click(function());     Equivale a onclick
+    |  .mouseover(function()); Equivale a hover
+    |  .ready(function());     Equivale a onload
+    |
+  this
+  document
+  "#id"
+  ".class"
+  "p"       parrafos
+  "p<a"     anidados
+
+
+$(document).ready(
+  function(){
+    //codigo
+  }
+);
+
+
+Poner la libreria y luego el enlace al archivo de .js, en el head. OJO pueden necesitarse mas librerias.
+https://developers.google.com/speed/libraries
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="./script/script_main.js"></script>
+
+Lo de descargar en Excel es un plugin de jQuery, buscar.
