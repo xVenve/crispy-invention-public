daf60ca4919251098229c768225db720a23aff33
CarlosCRO <carlosrubiolivares2000@gmail.com>
2020-10-23 18:44:53 +0200

    IU Practica 2 1.4 #26
    Implementación de pop ups con calendarios
    
    
    Former-commit-id: 7246536301b22f1e6c1f88e05077299101df62bd

--- IU/Practica 2/ej/script/script_main.js
+++ IU/Practica 2/ej/script/script_main.js
@@ -1,3 +1,115 @@
+$(document).ready(function() {
+  $('#NOVEDADES').on('click', function() {
+    $('#popup').fadeIn('slow');
+    $('.popup-overlay').fadeIn('slow');
+    $('.popup-overlay').height($(window).height());
+    return false;
+  });
+  $('.cerrar').on('click', function() {
+    $('#popup').fadeOut('slow');
+    $('.popup-overlay').fadeOut('slow');
+    return false;
+  });
+});
+
+$(document).ready(function() {
+  $('#NOTICIAS').on('click', function() {
+    $('#popup2').fadeIn('slow');
+    $('.popup-overlay').fadeIn('slow');
+    $('.popup-overlay').height($(window).height());
+    return false;
+  });
+  $('.cerrar').on('click', function() {
+    $('#popup2').fadeOut('slow');
+    $('.popup-overlay').fadeOut('slow');
+    return false;
+  });
+});
+
+
+
+
+
+$(function(){
+  $("#calendar_novedades").simpleCalendar();
+});
+
+
+$("#calendar_novedades").simpleCalendar({
+
+  // displays events
+  displayEvent: true,
+
+  // event dates
+  events: [
+    {
+      startDate: new Date("2020-10-1"),
+      endDate: new Date("2020-10-1"),
+      summary: 'Nuevo video subido: La historia de la web'
+    },
+    {
+      startDate: new Date("2020-09-29"),
+      endDate: new Date("2020-09-29"),
+      summary: 'Nuevo articulo subido: Introduccion a HTML'
+    },
+    {
+      startDate: new Date("2020-9-18"),
+      endDate: new Date("2020-9-18"),
+      summary: 'Foro de la asignatura habilitado'
+    },
+    {
+      startDate: new Date("2020-9-14"),
+      endDate: new Date("2020-9-14"),
+      summary: 'Bienvenida a la asignatura'
+    }
+  ],
+
+  // disable showing event details
+  disableEventDetails: false,
+
+  // disable showing empty date details
+  disableEmptyDetails: true
+
+});
+
+$(function(){
+  $("#calendar_noticias").simpleCalendar();
+});
+
+
+$("#calendar_noticias").simpleCalendar({
+
+  // displays events
+  displayEvent: true,
+
+  // event dates
+  events: [
+    {
+      startDate: new Date("2020-10-5"),
+      endDate: new Date("2020-10-5"),
+      summary: 'Entrega practica en dos días'
+    },
+    {
+      startDate: new Date("2020-10-1"),
+      endDate: new Date("2020-10-1"),
+      summary: 'Clase online magistral para el dia 3 de octubre'
+    },
+    {
+      startDate: new Date("2020-9-28"),
+      endDate: new Date("2020-9-28"),
+      summary: 'Tutoria colectiva en tres días'
+    }
+  ],
+
+  // disable showing event details
+  disableEventDetails: false,
+
+  // disable showing empty date details
+  disableEmptyDetails: true
+
+
+});
+
 document.addEventListener("DOMContentLoaded", start());
 
 function start() {
