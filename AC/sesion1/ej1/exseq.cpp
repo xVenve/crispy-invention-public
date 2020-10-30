e0b0bbb39b9f3637e034d4c9001b489b803486d4
Jorge Rodríguez <jrf1616@gmail.com>
2020-10-30 15:11:38 +0100

    AC Sesion 1 OpenMP
    Un par de ejemplos de las diapositivas
    
    
    Former-commit-id: 18622eff5617a04aedb30669263b0b4a2f0cd0d9

---
+++ AC/sesion1/ej1/exseq.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+  using namespace std;
+
+  cout << "Hilos antes del pragma " << omp_get_num_threads();
+
+#pragma omp parallel num_threads(4)
+  //  cout <<"Hilos justo despues del pragma"<< omp_get_num_threads();
+  {
+    int id = omp_get_thread_num();
+    cout << "Hola(" << id << ") ";
+    cout << "Mundo(" << id << ") ";
+    cout << "Hilos en el bloque de pragma " << omp_get_num_threads();
+  }
+
+  cout << "Hilos del fin de programa " << omp_get_num_threads();
+  return 0;
+}
