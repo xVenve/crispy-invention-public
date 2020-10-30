9613f68358e0d576cefe1665bec741ceab21e095
Jorge Rodríguez <jrf1616@gmail.com>
2020-10-30 15:11:38 +0100

    AC Sesion 1 OpenMP
    Un par de ejemplos de las diapositivas
    
    
    Former-commit-id: db4fdfea552d7050a112b46cee363d7f4451fb85

---
+++ AC/sesion1/ej3/pi.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+
+  using namespace std;
+
+  constexpr long nsteps = 10000000;
+  double t1 = omp_get_wtime();
+  double step = 1.0 / double(nsteps);
+  double sum = 0.0;
+  int hilos = 0;
+  double vector[8];
+#pragma omp parallel
+  {
+    hilos = omp_get_num_threads();
+    for (int i = 0 + (nsteps / 8) * omp_get_thread_num(); i < (nsteps / 8) * (omp_get_thread_num() + 1); ++i) {
+      double x = (i + 0.5) * step;
+      vector[omp_get_thread_num()] += 4.0 / (1.0 + x * x);
+    }
+#pragma omp atomic
+    sum += vector[omp_get_thread_num()];
+  }
+
+  double pi = step * sum;
+
+  double t2 = omp_get_wtime();
+  double diff = t2 - t1;
+  cout << "Atomic:" << '\n';
+  cout << "Valor de pi: " << pi << '\n';
+  cout << "Numero de hilos: " << hilos << '\n';
+  cout << "Tiempo: " << diff << '\n';
+
+  sum = 0.0;
+  hilos = 0;
+#pragma omp parallel
+  {
+    hilos = omp_get_num_threads();
+    for (int i = 0 + (nsteps / 8) * omp_get_thread_num(); i < (nsteps / 8) * (omp_get_thread_num() + 1); ++i) {
+      double x = (i + 0.5) * step;
+      vector[omp_get_thread_num()] += 4.0 / (1.0 + x * x);
+    }
+#pragma omp critical
+    sum += vector[omp_get_num_threads()];
+  }
+
+  pi = step * sum;
+
+  t2 = omp_get_wtime();
+  diff = t2 - t1;
+  cout << "Critical:" << '\n';
+  cout << "Valor de pi: " << pi << '\n';
+  cout << "Numero de hilos: " << hilos << '\n';
+  cout << "Tiempo: " << diff << '\n';
+}
---
+++ AC/sesion1/ej4/pi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+
+  using namespace std;
+
+  constexpr long nsteps = 10000000;
+  double t1 = omp_get_wtime();
+  double step = 1.0 / double(nsteps);
+  double sum = 0.0;
+  int hilos = 0;
+#pragma omp parallel
+  {
+    hilos = omp_get_num_threads();
+#pragma omp for reduction(+ : sum)
+    for (int i = 0; i < nsteps; ++i) {
+      double x = (i + 0.5) * step;
+      sum += 4.0 / (1.0 + x * x);
+    }
+  }
+  double pi = step * sum;
+
+  double t2 = omp_get_wtime();
+  double diff = t2 - t1;
+  cout << "Valor de pi: " << pi << '\n';
+  cout << "Numero de hilos: " << hilos << '\n';
+  cout << "Tiempo: " << diff << '\n';
+}
