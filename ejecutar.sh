e82e2aefc1731847a664c0e0e28f63ae8ce061f9
Jorge Rodríguez <jrf1616@gmail.com>
2020-11-04 22:14:58 +0100

    AC Lab2 1.0.2
    Script compilar y ejecutar
    
    
    Former-commit-id: 076528be448283845806ee38b982f80947e61271

---
+++ ejecutar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+MAGENTA='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${MAGENTA}Compilando...$NC"
+
+make
+
+echo -e "${MAGENTA}Introduce la operacion:$NC"
+
+read func
+
+echo -e "${MAGENTA}Ejecutando...$NC"
+
+./image-seq $func input output
+
+echo -e "${MAGENTA}Completado$NC"
