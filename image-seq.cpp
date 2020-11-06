98033786d79fe2e17f30922c2e0d59391664e407
Jorge Rodríguez <jrf1616@gmail.com>
2020-11-06 15:23:58 +0100

    AC Lab2 1.1
    Lectura de los archivos del directorio de entrada.
    Fotos de ejemplo añadidas
    
    
    Former-commit-id: a1be27597a084f73b01d5e898421c455453a9f1a

--- image-seq.cpp
+++ image-seq.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 using namespace std;
 
+void copy(char *, DIR *);
+void gauss(char *, DIR *);
+void sobel(char *, DIR *);
+
 int main(int argc, char **argv) {
 
   // Controlar el numero de parametros
@@ -41,9 +45,53 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  // Mirar error si esta vacio el de entrada
+  // Mirar error si esta vacio el de entrada, o archivo con otro formato
+
+  if (strcmp(argv[1], "copy") == 0) {
+    struct dirent *dir;
+    while ((dir = readdir(input)) != NULL) {
+      if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+        cout << dir->d_name << "\n";
+        copy(dir->d_name, output);
+      }
+    }
+  }
+
+  if (strcmp(argv[1], "gauss") == 0) {
+    struct dirent *dir;
+    while ((dir = readdir(input)) != NULL) {
+      if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+        cout << dir->d_name << "\n";
+        gauss(dir->d_name, output);
+      }
+    }
+  }
+
+  if (strcmp(argv[1], "sobel") == 0) {
+    struct dirent *dir;
+    while ((dir = readdir(input)) != NULL) {
+      if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+        cout << dir->d_name << "\n";
+        // Funcion sobel
+      }
+    }
+  }
 
   closedir(input);
   closedir(output);
   return 0;
+}
+
+void copy(char *photo, DIR *output) {
+  cout << "Copio:" << photo << " en " << output << "\n";
+}
+
+void gauss(char *photo, DIR *output) {
+  cout << "Gauss:" << photo << " en " << output << "\n";
+}
+
+void sobel(char *photo, DIR *output) {
+  // Ver como hacer que haga primero gauss, doble funcion en una o que reciba
+  // parametros
+  cout << "Gauss y Sobel:" << photo << " en " << output << "\n";
 }
