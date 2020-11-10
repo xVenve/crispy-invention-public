37479cd85a6745511299b848c959358ff0ce831a
Jorge Rodríguez <jrf1616@gmail.com>
2020-11-10 21:53:29 +0100

    AC Lab2 1.2
    Actualizada la memoria y subido el ejecutable de la solucion.
    En lo comentado hay un prototipo de leer datos y cabecera.
    
    
    Former-commit-id: e2655a39df9a31e6053f461baed4ef4520a410b5

--- image-seq.cpp
+++ image-seq.cpp
@@ -6,12 +6,14 @@ using namespace std;
 void copy(char *, DIR *);
 void gauss(char *, DIR *);
 void sobel(char *, DIR *);
+// int check(unsigned char *);
 
 int main(int argc, char **argv) {
 
   // Controlar el numero de parametros
   if (argc < 4) {
-    cout << "Wrong format:\n  image-seq operation in_path out_path\n   "
+    cout << "Wrong format:\n " << argv[0]
+         << " operation in_path out_path\n   "
             "operation: copy, gauss, sobel\n";
     return -1;
   }
@@ -19,8 +21,9 @@ int main(int argc, char **argv) {
   // Comprobar que la operacion sea una de las admitidas
   if (strcmp(argv[1], "gauss") && strcmp(argv[1], "copy") &&
       strcmp(argv[1], "sobel")) {
-    cout << "Unexpected operation:" << argv[1]
-         << "\n  image-seq operation in_path out_path\n   operation: copy, "
+    cout << "Unexpected operation:" << argv[1] << "\n"
+         << argv[0]
+         << " operation in_path out_path\n   operation: copy, "
             "gauss, sobel\n";
     return -1;
   }
@@ -30,7 +33,9 @@ int main(int argc, char **argv) {
   if (input == NULL) {
     cout << "Input path: " << argv[2] << "\nOutput path: " << argv[3]
          << "\nCannot open directory [" << argv[2] << "]"
-         << "\n  image-seq operation in_path out_path\n   operation: copy, "
+         << "\n"
+         << argv[0]
+         << " operation in_path out_path\n   operation: copy, "
             "gauss, sobel\n";
     return -1;
   }
@@ -40,20 +45,53 @@ int main(int argc, char **argv) {
   if (output == NULL) {
     cout << "Input path: " << argv[2] << "\nOutput path: " << argv[3]
          << "\nOutput directory [" << argv[2] << "] does not exist"
-         << "\n  image-seq operation in_path out_path\n   operation: copy, "
+         << "\n"
+         << argv[0]
+         << " operation in_path out_path\n   operation: copy, "
             "gauss, sobel\n";
     return -1;
   }
 
   // Mirar error si esta vacio el de entrada, o archivo con otro formato
 
-  if (strcmp(argv[1], "copy") == 0) {
-    struct dirent *dir;
-    while ((dir = readdir(input)) != NULL) {
-      if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-        cout << dir->d_name << "\n";
+  struct dirent *dir;
+  while ((dir = readdir(input)) != NULL) {
+    if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+      cout << dir->d_name << "\n";
+
+      /* Sacar datos de .bmp, pero mirar paso de nombre
+
+      FILE *photo = fopen(dir->d_name, "rb");
+      unsigned char info[54];
+
+      // Cabecera
+      fread(info, sizeof(unsigned char), 54, photo);
+
+      // Ancho y alto
+      int width = *(int *)&info[18];
+      int height = *(int *)&info[22];
+
+      // 24 bits = 3 bytes por pixel
+      int size = 3 * width * height;
+      unsigned char *data = new unsigned char[size];
+
+      fread(data, sizeof(unsigned char), size, photo);
+
+      if (check(info) == -1) {
+        return -1;
+      }
+      */
+
+      if (strcmp(argv[1], "copy") == 0) {
         copy(dir->d_name, output);
       }
+      if (strcmp(argv[1], "gauss") == 0) {
+        gauss(dir->d_name, output);
+      }
+      if (strcmp(argv[1], "sobel") == 0) {
+        // Funcion sobel
+      }
+      // fclose(photo);
     }
   }
 
@@ -95,3 +133,14 @@ void sobel(char *photo, DIR *output) {
   // parametros
   cout << "Gauss y Sobel:" << photo << " en " << output << "\n";
 }
+
+// int check(unsigned char* header) {
+/*Comprobar los parametros de las fotos
+Input path: input
+Output path: output
+Bit count is not 24
+./solucion operation in_path out_path
+  operation: copy, gauss, sobel
+*/
+// return 0;
+//}
