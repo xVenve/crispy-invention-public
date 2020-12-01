79c23a363f379e6616d082b9d4f0bed69aef1823
Francisco_1903 <franciscojr2000@gmail.com>
2020-12-01 18:53:58 +0100

    Cambios en gauss: hemos comentado lo que hicisteis Carlos y Jorge y lo que he añadido lo he comentado con /*nuestro*/ delante, he borrado algunas condiciones del if de los i+s y demás y en las variables x, y, z tambien he borrado la variable pad que sumabais
    Former-commit-id: e5e3096ef4fc6d16f554041d82c8df2b1119b165

--- image-seq.cpp
+++ image-seq.cpp
@@ -261,10 +261,9 @@ int main(int argc, char **argv) {
         int my[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
 
         unsigned char *sobel = new unsigned char[size];
-        int pad = 0;
+
         auto sobeltimei = clk ::now();
         for (int i = 0; i < height; i++) {
-          pad += (width % 4);
           for (int j = 0; j < width; j++) {
             int xx = 0;
             int yx = 0;
@@ -276,27 +275,28 @@ int main(int argc, char **argv) {
             for (int s = -1; s < 2; s++) {
               for (int t = -1; t < 2; t++) {
                 // Condición para marcado de bordes
-                if ((i + s) < height && (j + t) < width && (i + s) >= 0 &&
-                    (j + t + pad) >= 0) {
+                if ((i + s) <= height - 1 && (j + t) <= width - 1 &&
+                    (i + s) >= 0 && (j + t) >= 0) {
                   xx += mx[s + 1][t + 1] *
-                        gaussres[3 * ((i + s) * width + (j + t)) + pad];
+                        gaussres[3 * ((i + s) * width + (j + t))];
                   yx += mx[s + 1][t + 1] *
-                        gaussres[3 * ((i + s) * width + (j + t)) + 1 + pad];
+                        gaussres[3 * ((i + s) * width + (j + t)) + 1];
                   zx += mx[s + 1][t + 1] *
-                        gaussres[3 * ((i + s) * width + (j + t)) + 2 + pad];
+                        gaussres[3 * ((i + s) * width + (j + t)) + 2];
 
                   xy += my[s + 1][t + 1] *
-                        gaussres[3 * ((i + s) * width + (j + t)) + pad];
+                        gaussres[3 * ((i + s) * width + (j + t))];
                   yy += my[s + 1][t + 1] *
-                        gaussres[3 * ((i + s) * width + (j + t)) + 1 + pad];
+                        gaussres[3 * ((i + s) * width + (j + t)) + 1];
                   zy += my[s + 1][t + 1] *
-                        gaussres[3 * ((i + s) * width + (j + t)) + 2 + pad];
+                        gaussres[3 * ((i + s) * width + (j + t)) + 2];
                 }
               }
             }
-            sobel[3 * (i * width + j) + pad] = abs(xx / 8) + abs(xy / 8);
-            sobel[3 * (i * width + j) + 1 + pad] = abs(yx / 8) + abs(yy / 8);
-            sobel[3 * (i * width + j) + 2 + pad] = abs(zx / 8) + abs(zy / 8);
+
+            sobel[3 * (i * width + j)] = abs(zx / 8) + abs(zy / 8);
+            sobel[3 * (i * width + j) + 1] = abs(yx / 8) + abs(yy / 8);
+            sobel[3 * (i * width + j) + 2] = abs(zx / 8) + abs(zy / 8);
           }
         }
         auto sobeltimef = clk ::now();
@@ -369,9 +369,14 @@ void gauss(int width, int height, unsigned char *data, unsigned char *res) {
 
   // int row_padded = (width*3+3) & (~3);
   // unsigned char* padded_data = new unsigned char [rowpadded];
-  int pad = 0;
+  int pad = (width%4);
+  //int cont = 1;
   for (int i = 0; i < height; i++) {
-    pad += (width % 4);
+  /*  if (cont == 3) {
+      pad += (width % 4);
+      cont = 0;
+    }
+      cont++;*/
     for (int j = 0; j < width; j++) {
       int x = 0;
       int y = 0;
@@ -380,18 +385,21 @@ void gauss(int width, int height, unsigned char *data, unsigned char *res) {
         for (int t = -2; t < 3; t++) {
           // Condición para marcado de bordes j <= (width%4)*4
           if ((i + s) < height && (j + t) < width && (i + s) >= 0 &&
-              (j + t + pad) >= 0) {
-            x += m[s + 2][t + 2] * data[3 * ((i + s) * width + (j + t)) + pad];
+              (j + t) >= 0) {
+            x += m[s + 2][t + 2] * data[3 * ((i + s) * width + (j + t))];
             y += m[s + 2][t + 2] *
-                 data[3 * ((i + s) * width + (j + t)) + pad + 1];
+                 data[3 * ((i + s) * width + (j + t)) + 1];
             z += m[s + 2][t + 2] *
-                 data[3 * ((i + s) * width + (j + t)) + pad + 2];
+                 data[3 * ((i + s) * width + (j + t)) + 2];
           }
         }
       }
-      res[3 * (i * width + j) + pad] = x / 273;
-      res[3 * (i * width + j) + 1 + pad] = y / 273;
-      res[3 * (i * width + j) + 2 + pad] = z / 273;
+      res[3 * (i * width + j)] = data[3 * (i  * width + j)];
+      res[3 * (i * width + j)+1] = data[3 * (i  * width + j)+1];
+      res[3 * (i * width + j)+2] = data[3 * (i  * width + j)+2];
+      res[3 * (i * width + j - pad)] = x / 273;
+      res[3 * (i * width + j - pad) + 1] = y / 273;
+      res[3 * (i * width + j - pad) + 2] = z / 273;
     }
   }
 }
