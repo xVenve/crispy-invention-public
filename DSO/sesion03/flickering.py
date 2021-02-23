3395e416bc0582a7b7971b6f10c7db4917c477af
Jorge Rodríguez <jrf1616@gmail.com>
2021-02-23 23:05:27 +0100

    DSO Sesion03 1.1
    Añadido el enunciado y formateado

--- DSO/sesion03/flickering.py
+++ DSO/sesion03/flickering.py
@@ -3,11 +3,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(23,GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 for i in range(50):
-    GPIO.output(23,GPIO.HIGH)
+    GPIO.output(23, GPIO.HIGH)
     time.sleep(.02)
-    GPIO.output(23,GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
     time.sleep(.02)
-
-
