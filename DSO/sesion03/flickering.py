c82552e8f62ba07f1e7161efc09353b29eaa3e58
Jorge Rodríguez <jrf1616@gmail.com>
2021-02-23 21:59:03 +0100

    DSO Sesion03 1.0
    Primer programa para probar la raspberry con Python.
       ping.py Sacar el ping
       led.py Enciende un led 1 segundo
       flickering.py Hace parpadear el led.

---
+++ DSO/sesion03/flickering.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23,GPIO.OUT)
+for i in range(50):
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(.02)
+    GPIO.output(23,GPIO.LOW)
+    time.sleep(.02)
+
+
