ef8aff1db717081c9b881a6dd03d147b4737a051
Jorge Rodríguez <jrf1616@gmail.com>
2021-03-06 13:03:22 +0100

    DSO Sesion04 1.1
    Added comments and docs

--- DSO/sesion04/sensorDistancia.py
+++ DSO/sesion04/sensorDistancia.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
+# set GPIO Pins
 pingTrigger = 18
 pinEcho = 24
 
@@ -15,26 +16,33 @@ def close(signal, frame):
 
 signal.signal(signal.SIGINT, close)
 
+# set GPIO input and output chanels
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pingTrigger, GPIO.OUT)
 GPIO.setup(pinEcho, GPIO.IN)
 
 while True:
+    # set Trigger to HIGH
     GPIO.output(pingTrigger, True)
+    # set Trigger afete 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(pingTrigger, False)
 
     startTime = time.time()
     stopTime = time.time()
-
+    
+    # save start time
     while 0 == GPIO.input(pinEcho):
         startTime = time.time()
 
+    # save time of arrival
     while 1 == GPIO.input(pinEcho):
         stopTime = time.time()
 
+    # time difference between start and arrival
     TimeElapse = stopTime - startTime
-
+    # multiply with the sonic speec (34300 cm/s)
+    # and divide by 2, because there and back
     distance = (TimeElapse * 34300) / 2
 
     print("Distance: %.1f cm" % distance)
