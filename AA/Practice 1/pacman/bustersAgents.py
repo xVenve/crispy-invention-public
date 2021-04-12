26cc9c94e608af98c6c5fce06395a5eb9d38a6ab
Jorge Rodríguez <jrf1616@gmail.com>
2021-04-12 21:02:48 +0200

    AA Practice 1 1.8.1
    Entrega II

--- AA/Practice 1/pacman/bustersAgents.py
+++ AA/Practice 1/pacman/bustersAgents.py
@@ -309,8 +309,16 @@ class BasicAgentAA(BustersAgent):
         # numpy.count_nonzero(gameState.getLivingGhosts()),
         # gameState.getScore()]
 
-        move = self.weka.predict("./kstar.model",x,"./training_tutorial1Python.arff")
-        if move not in legal: move = Directions.STOP
+        move = self.weka.predict("./J48.model",x,"./training_keyboardPython.arff")
+        if move not in legal or move == Directions.STOP:
+            condition = 0
+            while condition == 0:
+                move_random = random.randint(0, 3)
+                if   ( move_random == 0 ) and Directions.WEST in legal: move = Directions.WEST
+                if   ( move_random == 1 ) and Directions.EAST in legal: move = Directions.EAST
+                if   ( move_random == 2 ) and Directions.NORTH in legal: move = Directions.NORTH
+                if   ( move_random == 3 ) and Directions.SOUTH in legal: move = Directions.SOUTH
+                if   move in legal: condition = 1
         return move
 
     def printLineData(self, gameState, gameState2):
