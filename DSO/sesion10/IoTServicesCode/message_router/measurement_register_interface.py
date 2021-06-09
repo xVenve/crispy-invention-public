20885fa07b05a8417776c2bfe596b734f26e6ce5
Jorge Rodríguez <jrf1616@gmail.com>
2021-06-10 01:03:28 +0200

    DSO Sesion10 1.1

--- DSO/sesion10/IoTServicesCode/message_router/measurement_register_interface.py
+++
@@ -1,8 +0,0 @@
-import requests
-from utils.load_preferences import getPreferences
-
-
-def submit_data_to_store(data):
-    params = getPreferences("measurements_microservice_connector.yaml")
-    response = requests.post('http://' + params["server"] + ':' + params["port"] + '/measurements/register', json=data)
-    return response.content
